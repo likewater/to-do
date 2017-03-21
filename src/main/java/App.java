1299183c79488c1c9161efd044e8a2979f959224
Blake Womack and Grace Stuart <grace@>
2017-03-21 09:44:15 -0700

    add import list and arraylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -18,19 +20,16 @@ public class App {
 
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-
       // include an if conditional that attempts to retrieve an ArrayList from the session saved under the key "tasks". If that ArrayList does not exist yet, we create a new one and add it to the session
       ArrayList<Task> tasks = request.session().attribute("tasks");
         if (tasks == null) {
           tasks = new ArrayList<Task>();
           request.session().attribute("tasks", tasks);
         }
-
       // create our Task object and add it into the tasks ArrayList with: tasks.add(newTask);
       String description = request.queryParams("description");
       Task newTask = new Task(description);
       tasks.add(newTask); // adding the task to the arrayList
-
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
